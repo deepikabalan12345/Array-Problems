1dac71ccb0c45c8f0e030ed2f7c17d112b979271
deepikabalan12345 <157381325+deepikabalan12345@users.noreply.github.com>
2024-07-31 16:55:20 +0530

    Add files via upload

---
+++ duplicate.js
@@ -0,0 +1,3 @@
+const array = [1, 2, 2, 3, 4, 4, 5,5];
+const Array = [...new Set(array)];
+console.log(Array);
---
+++ removing item.js
@@ -0,0 +1,6 @@
+let fruits = ['apple', 'banana', 'cherry', 'date'];
+let indexToRemove = 1;
+
+fruits.splice(indexToRemove, 1);
+
+console.log(fruits); 
